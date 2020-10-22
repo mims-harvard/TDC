83c1d4e161cd4cd8d55dbc6d75ce63ec537f1596
futianfan <futianfan@gmail.com>
2020-10-22 17:26:35 -0400

    generation_dataset print stats

--- tdc/generation/generation_dataset.py
+++ tdc/generation/generation_dataset.py
@@ -15,7 +15,7 @@ class DataLoader(base_dataset.DataLoader):
 			name: fuzzy name of the generation dataset. e.g., uspto50k, qed, drd, ... 
 			path: directory path that stores the dataset, e.g., ./data
 			print_stats: bool, whether print the stats.  
-			dataset_names: exact names of dataset  e.g., 
+			dataset_names: exact names of dataset  e.g., ['uspto50k', 'qed', 'drd2', 'logp']
 		returns:
 			None
 		'''
@@ -30,5 +30,9 @@ class DataLoader(base_dataset.DataLoader):
 	# def __init__(self, name, path, print_stats, dataset_names):
 	# 	if name.lower() in retrosyn_dataset_names.keys():  
 	# 		print_sys("Tip: Use tdc.utils.target_list('" + name.lower() + "') to retrieve all available label targets.")
+
+	def print_info(self):
+		print("There are " + str(len(self.input_smiles)) + ' paired samples', flush = True, file = sys.stderr)
+
 
 
