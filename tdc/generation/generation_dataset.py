8c73dd3ab53e2c62ac4a9f55912e8ec8c0febb06
futianfan <futianfan@gmail.com>
2020-10-22 17:09:13 -0400

    generate_dataset

--- tdc/generation/generation_dataset.py
+++ tdc/generation/generation_dataset.py
@@ -9,9 +9,20 @@ from ..utils import *
 
 
 class DataLoader(base_dataset.DataLoader):
+	def __init__(self, name, path, target, print_stats, dataset_names):
+		'''
+		Arguments:
+			name: fuzzy name of the generation dataset. e.g., uspto50k, qed, drd, ... 
+			path: directory path that stores the dataset, e.g., ./data
+			print_stats: bool, whether print the stats.  
+			dataset_names: exact names of dataset  e.g., 
+		returns:
+			None
+		'''
+		pass 
+
 	# def __init__(self, name, path, print_stats, dataset_names):
 	# 	if name.lower() in retrosyn_dataset_names.keys():  
 	# 		print_sys("Tip: Use tdc.utils.target_list('" + name.lower() + "') to retrieve all available label targets.")
 
-	pass 
 
