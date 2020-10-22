c740194b65f846e105511852ca8323721b4c4fcc
futianfan <futianfan@gmail.com>
2020-10-21 23:41:21 -0400

    generation:scratch

---
+++ tdc/generation/generation_dataset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import os, sys, json 
+import warnings
+warnings.filterwarnings("ignore")
+
+from .. import base_dataset
+from ..utils import *
+
+
+class DataLoader(base_dataset.DataLoader):
+	# def __init__(self, name, path, print_stats, dataset_names):
+	# 	if name.lower() in retrosyn_dataset_names.keys():  
+	# 		print_sys("Tip: Use tdc.utils.target_list('" + name.lower() + "') to retrieve all available label targets.")
+
+	pass 
+
