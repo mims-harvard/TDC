11a179bdc6aace56aa8a35531f8276ac6f7593af
Yue Zhao <zhaoy@cmu.edu>
2020-10-23 15:16:05 -0400

    pre-release setup

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(name='DrugDataLoader',
-      version='0.1',
+# read the contents of README file
+from os import path
+from io import open  # for Python 2 and 3 compatibility
+
+# get __version__ from _version.py
+ver_file = path.join('tdc', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+this_directory = path.abspath(path.dirname(__file__))
+
+
+# read the contents of README.md
+def readme():
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+
+# read the contents of requirements.txt
+with open(path.join(this_directory, 'requirements.txt'),
+          encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(name='TDC',
+      version=__version__,
       description='drug data loader',
-      url='https://github.com/kexinhuang12345/DrugDataLoader',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/mims-harvard/TDC',
       author='Kexin Huang',
       author_email='kexinhuang@hsph.harvard.edu',
-      license='MIT',
-      packages=['DrugDataLoader'],
+      license='CC BY-NC-SA 4.0',
+      packages=find_packages(exclude=['test']),
       zip_safe=False,
-      install_requires=['numpy','pandas','wget','tqdm','fuzzywuzzy'])
-
+      include_package_data=True,
+      install_requires=requirements,
+      setup_requires=['setuptools>=38.6.0']
+      )
