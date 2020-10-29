53e1d2d6211b0b6fb5056382193ebd3c604ca8b8
Kexin Huang <kexinhuang@hsph.harvard.edu>
2020-10-29 11:48:11 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,3 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+$(document).ready(function() {
+  $('#tabs li').on('click', function() {
+    var tab = $(this).data('tab');
+
+    $('#tabs li').removeClass('is-active');
+    $(this).addClass('is-active');
+
+    $('#tab-content p').removeClass('is-active');
+    $('p[data-content="' + tab + '"]').addClass('is-active');
+  });
+});
