d61aea7922c5a2eb541a26ce72bda4daa4d34a2e
futianfan <futianfan@gmail.com>
2020-11-04 22:08:00 -0500

    forward or retro synthetic test

---
+++ tests/test_forward_synthetic.py
@@ -0,0 +1,14 @@
+from tdc.generation import ForwardSyn, Retro_Syn
+
+
+paired_dataloader = ForwardSyn(name = 'uspto50k')
+
+pd1 = paired_dataloader.get_data()
+split_pd = paired_dataloader.get_split()
+
+
+paired_dataloader2 = Retro_Syn(name = 'uspto50k')
+pd2 = paired_dataloader2.get_data()
+
+
+
