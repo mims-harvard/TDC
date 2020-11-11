e027d40d55ab80ea1747642cf4daf324b237deb2
futianfan <futianfan@gmail.com>
2020-11-10 22:04:08 -0500

    merge tdc/sascorer into tdc/chem_utils

--- tdc/chem_utils.py
+++ tdc/chem_utils.py
@@ -2,6 +2,9 @@ import pickle
 import numpy as np 
 import re
 import os.path as op
+import math
+from collections import defaultdict
+
 try:
 	from sklearn import svm
 	# from sklearn.metrics import roc_auc_score, f1_score, average_precision_score, precision_score, recall_score, accuracy_score
@@ -16,6 +19,9 @@ try:
 	import rdkit.Chem.QED as QED
 	from rdkit import rdBase
 	rdBase.DisableLog('rdApp.error')
+	from rdkit.Chem import rdMolDescriptors
+	from rdkit.six.moves import cPickle
+	from rdkit.six import iteritems
 
 except:
 	raise ImportError("Please install rdkit by 'conda install -c conda-forge rdkit'! ")	
@@ -26,11 +32,11 @@ except:
 # import sascorer
 try:
 	from .score_modifier import *
-	from .sascorer import * 
+	# from .sascorer import * 
 	# from .drd2_scorer import get_score as drd2 
 except:
 	from score_modifier import *
-	from sascorer import * 
+	# from sascorer import * 
 	# from drd2_scorer import get_score as drd2 
 
 
@@ -42,6 +48,86 @@ except:
 from .utils import oracle_load
 
 
+
+
+
+
+
+_fscores = None
+def readFragmentScores(name='fpscores'):
+    import gzip
+    global _fscores
+    # generate the full path filename:
+    # if name == "fpscores":
+    #     name = op.join(previous_directory(op.dirname(__file__)), name)
+    name = "tdc/fpscores"
+    _fscores = cPickle.load(gzip.open('%s.pkl.gz'%name))
+    outDict = {}
+    for i in _fscores:
+        for j in range(1,len(i)):
+            outDict[i[j]] = float(i[0])
+    _fscores = outDict
+
+def numBridgeheadsAndSpiro(mol,ri=None):
+  nSpiro = rdMolDescriptors.CalcNumSpiroAtoms(mol)
+  nBridgehead = rdMolDescriptors.CalcNumBridgeheadAtoms(mol)
+  return nBridgehead,nSpiro
+
+def calculateScore(m):
+  if _fscores is None: readFragmentScores()
+
+  # fragment score
+  fp = rdMolDescriptors.GetMorganFingerprint(m,2)  #<- 2 is the *radius* of the circular fingerprint
+  fps = fp.GetNonzeroElements()
+  score1 = 0.
+  nf = 0
+  for bitId,v in iteritems(fps):
+    nf += v
+    sfp = bitId
+    score1 += _fscores.get(sfp,-4)*v
+  score1 /= nf
+
+  # features score
+  nAtoms = m.GetNumAtoms()
+  nChiralCenters = len(Chem.FindMolChiralCenters(m,includeUnassigned=True))
+  ri = m.GetRingInfo()
+  nBridgeheads,nSpiro=numBridgeheadsAndSpiro(m,ri)
+  nMacrocycles=0
+  for x in ri.AtomRings():
+    if len(x)>8: nMacrocycles+=1
+
+  sizePenalty = nAtoms**1.005 - nAtoms
+  stereoPenalty = math.log10(nChiralCenters+1)
+  spiroPenalty = math.log10(nSpiro+1)
+  bridgePenalty = math.log10(nBridgeheads+1)
+  macrocyclePenalty = 0.
+  # ---------------------------------------
+  # This differs from the paper, which defines:
+  #  macrocyclePenalty = math.log10(nMacrocycles+1)
+  # This form generates better results when 2 or more macrocycles are present
+  if nMacrocycles > 0: macrocyclePenalty = math.log10(2)
+
+  score2 = 0. -sizePenalty -stereoPenalty -spiroPenalty -bridgePenalty -macrocyclePenalty
+
+  # correction for the fingerprint density
+  # not in the original publication, added in version 1.1
+  # to make highly symmetrical molecules easier to synthetise
+  score3 = 0.
+  if nAtoms > len(fps):
+    score3 = math.log(float(nAtoms) / len(fps)) * .5
+
+  sascore = score1 + score2 + score3
+
+  # need to transform "raw" value into scale between 1 and 10
+  min = -4.0
+  max = 2.5
+  sascore = 11. - (sascore - min + 1) / (max - min) * 9.
+  # smooth the 10-end
+  if sascore > 8.: sascore = 8. + math.log(sascore+1.-9.)
+  if sascore > 10.: sascore = 10.0
+  elif sascore < 1.: sascore = 1.0 
+
+  return sascore
 
 
 
