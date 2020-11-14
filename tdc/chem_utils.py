4060d48548cfc4e478517de810b81f98be63ea3b
futianfan <futianfan@gmail.com>
2020-11-13 22:09:15 -0500

    kl divergence

--- tdc/chem_utils.py
+++ tdc/chem_utils.py
@@ -552,6 +552,184 @@ def diversity(list_of_smiles):
 			avg_lst.append(sim)
 	return np.mean(avg_lst)
 
+
+
+########################################
+######## KL divergence ########
+
+def _calculate_pc_descriptors(smiles: str, pc_descriptors: List[str]) -> np.array:
+    from rdkit.ML.Descriptors import MoleculeDescriptors
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator(pc_descriptors)
+
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return None
+    _fp = calc.CalcDescriptors(mol)
+    _fp = np.array(_fp)
+    mask = np.isfinite(_fp)
+    if (mask == 0).sum() > 0:
+        logger.warning(f'{smiles} contains an NAN physchem descriptor')
+        _fp[~mask] = 0
+
+    return _fp 
+
+def calculate_pc_descriptors(smiles: Iterable[str], pc_descriptors: List[str]) -> np.array:
+    output = []
+
+    for i in smiles:
+        d = _calculate_pc_descriptors(i, pc_descriptors)
+        if d is not None:
+            output.append(d)
+
+    return np.array(output)
+
+
+def continuous_kldiv(X_baseline: np.array, X_sampled: np.array) -> float:
+    from scipy.stats import entropy, gaussian_kde
+    kde_P = gaussian_kde(X_baseline)
+    kde_Q = gaussian_kde(X_sampled)
+    x_eval = np.linspace(np.hstack([X_baseline, X_sampled]).min(), np.hstack([X_baseline, X_sampled]).max(), num=1000)
+    P = kde_P(x_eval) + 1e-10
+    Q = kde_Q(x_eval) + 1e-10
+
+    return entropy(P, Q)
+
+def discrete_kldiv(X_baseline: np.array, X_sampled: np.array) -> float:
+    from scipy.stats import entropy
+    from scipy import histogram
+    P, bins = histogram(X_baseline, bins=10, density=True)
+    P += 1e-10
+    Q, _ = histogram(X_sampled, bins=bins, density=True)
+    Q += 1e-10
+
+    return entropy(P, Q)
+
+def get_fingerprints(mols: Iterable[Chem.Mol], radius=2, length=4096):
+    """
+    Converts molecules to ECFP bitvectors.
+
+    Args:
+        mols: RDKit molecules
+        radius: ECFP fingerprint radius
+        length: number of bits
+
+    Returns: a list of fingerprints
+    """
+    return [AllChem.GetMorganFingerprintAsBitVect(m, radius, length) for m in mols]
+
+
+def get_mols(smiles_list: Iterable[str]) -> Iterable[Chem.Mol]:
+    for i in smiles_list:
+        try:
+            mol = Chem.MolFromSmiles(i)
+            if mol is not None:
+                yield mol
+        except Exception as e:
+            logger.warning(e)
+
+
+
+def calculate_internal_pairwise_similarities(smiles_list: Collection[str]) -> np.array:
+    """
+    Computes the pairwise similarities of the provided list of smiles against itself.
+
+    Returns:
+        Symmetric matrix of pairwise similarities. Diagonal is set to zero.
+    """
+    if len(smiles_list) > 10000:
+        logger.warning(f'Calculating internal similarity on large set of '
+                       f'SMILES strings ({len(smiles_list)})')
+
+    mols = get_mols(smiles_list)
+    fps = get_fingerprints(mols)
+    nfps = len(fps)
+
+    similarities = np.zeros((nfps, nfps))
+
+    for i in range(1, nfps):
+        sims = DataStructs.BulkTanimotoSimilarity(fps[i], fps[:i])
+        similarities[i, :i] = sims
+        similarities[:i, i] = sims
+
+    return similarities
+
+
+
+def kldiv(generated_lst_smiles, training_lst_smiles):
+  pc_descriptor_subset = [
+            'BertzCT',
+            'MolLogP',
+            'MolWt',
+            'TPSA',
+            'NumHAcceptors',
+            'NumHDonors',
+            'NumRotatableBonds',
+            'NumAliphaticRings',
+            'NumAromaticRings'
+  ]
+
+  def canonical(smiles)
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is not None:
+        return Chem.MolToSmiles(mol, isomericSmiles=include_stereocenters)
+    else:
+        return None
+
+
+  generated_lst_mol = list(map(canonical, generated_lst_smiles))
+  training_lst_mol = list(map(canonical, training_lst_smiles))
+  filter_out_func = lambda x:x is not None 
+  generated_lst_mol = list(filter(filter_out_func, generated_lst_mol))
+  training_lst_mol = list(filter(filter_out_func, generated_lst_mol))
+
+  d_sampled = calculate_pc_descriptors(generated_lst_mol, pc_descriptor_subset)
+  d_chembl = calculate_pc_descriptors(training_lst_mol, pc_descriptor_subset)
+
+  for i in range(4):
+    kldiv = continuous_kldiv(X_baseline=d_chembl[:, i], X_sampled=d_sampled[:, i])
+    kldivs[pc_descriptor_subset[i]] = kldiv
+
+  # ... and for the int valued ones.
+  for i in range(4, 9):
+    kldiv = discrete_kldiv(X_baseline=d_chembl[:, i], X_sampled=d_sampled[:, i])
+    kldivs[pc_descriptor_subset[i]] = kldiv
+
+
+  # pairwise similarity
+
+  chembl_sim = calculate_internal_pairwise_similarities(self.training_set_molecules)
+  chembl_sim = chembl_sim.max(axis=1)
+
+  sampled_sim = calculate_internal_pairwise_similarities(unique_molecules)
+  sampled_sim = sampled_sim.max(axis=1)
+
+  kldiv_int_int = continuous_kldiv(X_baseline=chembl_sim, X_sampled=sampled_sim)
+  kldivs['internal_similarity'] = kldiv_int_int
+  '''
+        # for some reason, this runs into problems when both sets are identical.
+        # cross_set_sim = calculate_pairwise_similarities(self.training_set_molecules, unique_molecules)
+        # cross_set_sim = cross_set_sim.max(axis=1)
+        #
+        # kldiv_ext = discrete_kldiv(chembl_sim, cross_set_sim)
+        # kldivs['external_similarity'] = kldiv_ext
+        # kldiv_sum += kldiv_ext
+  '''
+  metadata = {
+            'number_samples': self.number_samples,
+            'kl_divs': kldivs
+  }
+
+  # Each KL divergence value is transformed to be in [0, 1].
+  # Then their average delivers the final score.
+  partial_scores = [np.exp(-score) for score in kldivs.values()]
+  score = sum(partial_scores) / len(partial_scores)
+  return score 
+
+##################################################
+#### End of distribution learning Evaluator
+##################################################
+
+
 def smiles_to_rdkit_mol(smiles):
 	mol = Chem.MolFromSmiles(smiles)
 	#  Sanitization check (detects invalid valence)
