244663af75629e5767ec4d66bf7a8658786879b4
Kexin Huang <kexinhuang@hsph.harvard.edu>
2021-08-13 08:41:01 -0700

    add bm docstring

--- tdc/benchmark_group/drugcombo_group.py
+++ tdc/benchmark_group/drugcombo_group.py
@@ -5,5 +5,13 @@
 from .base_group import BenchmarkGroup
 
 class drugcombo_group(BenchmarkGroup):
+	"""create a drug combination benchmark group
+		
+	Args:
+	    path (str, optional): path to save/load benchmarks
+	"""
+
 	def __init__(self, path = './data'):
+		"""create a drug combination benchmark group
+		"""
 		super().__init__(name = 'DrugCombo_Group', path = path, file_format = 'pkl')
--- tdc/benchmark_group/dti_dg_group.py
+++ tdc/benchmark_group/dti_dg_group.py
@@ -5,5 +5,12 @@
 from .base_group import BenchmarkGroup
 
 class dti_dg_group(BenchmarkGroup):
+	"""Create a DTI domain generalization benchmark group
+		
+	Args:
+	    path (str, optional): path to save/load benchmarks
+	"""
 	def __init__(self, path = './data'):
+		"""Create a DTI domain generalization benchmark group
+		"""
 		super().__init__(name = 'DTI_DG_Group', path = path, file_format = 'csv')
