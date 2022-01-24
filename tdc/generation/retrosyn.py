e62c890141c321f2f2433dfd5594ffc1ae4fb166
Kexin Huang <kexinhuang@hsph.harvard.edu>
2022-01-23 21:04:08 -0800

    add include_reaction_type parameter in the split

--- tdc/generation/retrosyn.py
+++ tdc/generation/retrosyn.py
@@ -7,6 +7,7 @@ warnings.filterwarnings("ignore")
 
 from . import generation_dataset
 from ..metadata import dataset_names
+from ..utils import create_fold
 
 class RetroSyn(generation_dataset.PairedDataLoader):
 
@@ -25,3 +26,33 @@ class RetroSyn(generation_dataset.PairedDataLoader):
 		    output_name (str, optional): the name of the column containing output molecular data (reactant)
 		"""
 		super().__init__(name, path, print_stats, input_name, output_name)
+
+	def get_split(self, method = 'random', seed = 42, frac = [0.7, 0.1, 0.2], include_reaction_type = False):
+		'''Return the data splitted as train, valid, test sets.
+
+		Arguments:
+		    method (str): splitting schemes: random, scaffold
+		    seed (int): random seed, default 42
+		    frac (list of float): ratio of train/val/test split
+		    include_reaction_type (bool): whether or not to include reaction type in the split
+		
+		Returns:
+		    pandas DataFrame/dict: a dataframe of the dataset
+		
+		Raises:
+		    AttributeError: Use the correct split method as input (random, scaffold)
+		'''
+		df = self.get_data(format = 'df')
+
+		if include_reaction_type:
+			from ..utils import get_reaction_type
+			try:
+				rt = get_reaction_type(self.name)
+				df['reaction_type'] = rt
+			except:
+				raise ValueError('Reaction Type Unavailable for ' + str(self.name) + '! Please turn include_reaction_type to be false!')
+		
+		if method == 'random':
+			return create_fold(df, seed, frac)
+		else:
+			raise AttributeError("Please use the correct split method")
