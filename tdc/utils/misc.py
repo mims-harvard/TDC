b32eddb6b5a453e729ac93fa9f2c449ef65174d8
George Dasoulas <george.dasoulas1@gmail.com>
2023-05-23 15:18:28 -0400

    support for transform h5py to pyg Data object

--- tdc/utils/misc.py
+++ tdc/utils/misc.py
@@ -6,7 +6,9 @@ import pandas as pd
 import subprocess
 import pickle
 from fuzzywuzzy import fuzz
-
+from torch_geometric.data import Data
+import h5py
+import torch 
 
 def fuzzy_search(name, dataset_names):
     """fuzzy matching between the real dataset name and the input name
@@ -132,3 +134,74 @@ def to_submission_format(results):
         return [round(np.mean(metric), 3), round(np.std(metric), 3)]
 
     return dict(df.apply(get_metric, axis=1))
+
+
+
+
+def h5py_to_pyg(self, h5_file_path):
+    h5File = h5py.File(h5_file_path, "r")
+    data = Data()
+
+    amino_types = h5File['amino_types'][()]
+    atom_names = h5File['atom_names'][()] 
+    atom_amino_id = h5File['atom_amino_id'][()] 
+    atom_pos = h5File['atom_pos'][()][0] 
+
+    # atoms to compute side chain torsion angles: N, CA, CB, _G/_G1, _D/_D1, _E/_E1, _Z, NH1
+    mask_n = np.char.equal(atom_names, b'N')
+    mask_ca = np.char.equal(atom_names, b'CA')
+    mask_c = np.char.equal(atom_names, b'C')
+    mask_cb = np.char.equal(atom_names, b'CB')
+    mask_g = np.char.equal(atom_names, b'CG') | np.char.equal(atom_names, b'SG') | np.char.equal(atom_names, b'OG') | np.char.equal(atom_names, b'CG1') | np.char.equal(atom_names, b'OG1')
+    mask_d = np.char.equal(atom_names, b'CD') | np.char.equal(atom_names, b'SD') | np.char.equal(atom_names, b'CD1') | np.char.equal(atom_names, b'OD1') | np.char.equal(atom_names, b'ND1')
+    mask_e = np.char.equal(atom_names, b'CE') | np.char.equal(atom_names, b'NE') | np.char.equal(atom_names, b'OE1')
+    mask_z = np.char.equal(atom_names, b'CZ') | np.char.equal(atom_names, b'NZ')
+    mask_h = np.char.equal(atom_names, b'NH1')
+
+    pos_n = np.full((len(amino_types),3),np.nan)
+    pos_n[atom_amino_id[mask_n]] = atom_pos[mask_n]
+    pos_n = torch.FloatTensor(pos_n)
+
+    pos_ca = np.full((len(amino_types),3),np.nan)
+    pos_ca[atom_amino_id[mask_ca]] = atom_pos[mask_ca]
+    pos_ca = torch.FloatTensor(pos_ca)
+
+    pos_c = np.full((len(amino_types),3),np.nan)
+    pos_c[atom_amino_id[mask_c]] = atom_pos[mask_c]
+    pos_c = torch.FloatTensor(pos_c)
+
+    # if data only contain pos_ca, we set the position of C and N as the position of CA
+    pos_n[torch.isnan(pos_n)] = pos_ca[torch.isnan(pos_n)]
+    pos_c[torch.isnan(pos_c)] = pos_ca[torch.isnan(pos_c)]
+
+    pos_cb = np.full((len(amino_types),3),np.nan)
+    pos_cb[atom_amino_id[mask_cb]] = atom_pos[mask_cb]
+    pos_cb = torch.FloatTensor(pos_cb)
+
+    pos_g = np.full((len(amino_types),3),np.nan)
+    pos_g[atom_amino_id[mask_g]] = atom_pos[mask_g]
+    pos_g = torch.FloatTensor(pos_g)
+
+    pos_d = np.full((len(amino_types),3),np.nan)
+    pos_d[atom_amino_id[mask_d]] = atom_pos[mask_d]
+    pos_d = torch.FloatTensor(pos_d)
+
+    pos_e = np.full((len(amino_types),3),np.nan)
+    pos_e[atom_amino_id[mask_e]] = atom_pos[mask_e]
+    pos_e = torch.FloatTensor(pos_e)
+
+    pos_z = np.full((len(amino_types),3),np.nan)
+    pos_z[atom_amino_id[mask_z]] = atom_pos[mask_z]
+    pos_z = torch.FloatTensor(pos_z)
+
+    pos_h = np.full((len(amino_types),3),np.nan)
+    pos_h[atom_amino_id[mask_h]] = atom_pos[mask_h]
+    pos_h = torch.FloatTensor(pos_h)
+
+    data.x = torch.unsqueeze(torch.tensor(amino_types),1)
+    data.coords_ca = pos_ca
+    data.coords_n = pos_n
+    data.coords_c = pos_c
+
+    h5File.close()
+    return data
