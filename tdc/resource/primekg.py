538e2d5219920178d6bb733a667d1329b4e8614b
abearab <abardesigner@gmail.com>
2023-08-30 16:46:24 -0700

    improve `get_nodes_by_source` function

--- tdc/resource/primekg.py
+++ tdc/resource/primekg.py
@@ -38,14 +38,22 @@ class PrimeKG:
         return G
 
     def get_nodes_by_source(self, source):
+        # extract x nodes
         x_df = self.df.query(
             f"x_source == '{source}' | y_source == '{source}'"
-        )[[col for col in self.df.columns if col.startswith("x_")]].rename(columns={col: col[2:]})
+        )[[col for col in self.df.columns if col.startswith("x_")]]
 
+        for col in x_df.columns:
+            x_df = x_df.rename(columns={col: col[2:]})
+
+        # extract y nodes
         y_df = self.df.query(
             f"x_source == '{source}' | y_source == '{source}'"
-        )[[col for col in self.df.columns if col.startswith("y_")]].rename(columns={col: col[2:]})
-
+        )[[col for col in self.df.columns if col.startswith("y_")]]
+        for col in y_df.columns:
+            if col.startswith("y_"):
+                x_df = x_df.rename(columns={col: col[2:]})
+        # merge x and y nodes and keep only unique nodes
         out = pd.concat([x_df, y_df], axis=1).query(f'source == "{source}"').drop_duplicates().reset_index(drop=True)
 
         return out
