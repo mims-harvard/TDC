c82937cf3ea4ccd58c00e620283a9db0b1f030b6
abearab <abardesigner@gmail.com>
2023-09-30 14:57:08 -0700

    add `knowledge_graph` module

---
+++ tdc/knowledge_graph.py
@@ -0,0 +1,70 @@
+"""A python module to build, handle, explore, and manipulate knowledge graphs.
+This is mimicking __PrimeKG__'s pandas dataframe format
+"""
+
+import pandas as pd
+
+from copy import copy
+
+kg_columns = [
+    'relation', 'display_relation',
+    'x_id', 'x_type', 'x_name', 'x_source',
+    'y_id', 'y_type', 'y_name', 'y_source'
+],
+
+
+class KnowledgeGraph:
+    def __init__(self, df=None):
+        if df is not None:
+            self.df = df
+        else:
+            self.df = pd.DataFrame('', columns=kg_columns)
+
+    def copy(self):
+        return copy(self)
+
+    def run_query(self, query):
+        """build subgraph using given query"""
+        self.df_raw = self.df
+        self.df = self.df.query(query)
+
+    def get_nodes_by_source(self, source):
+        # extract x nodes
+        x_df = self.df.query(
+            f"x_source == '{source}' | y_source == '{source}'"
+        )[[col for col in self.df.columns if col.startswith("x_")]]
+
+        for col in x_df.columns:
+            x_df = x_df.rename(columns={col: col[2:]})
+
+        # extract y nodes
+        y_df = self.df.query(
+            f"x_source == '{source}' | y_source == '{source}'"
+        )[[col for col in self.df.columns if col.startswith("y_")]]
+        for col in y_df.columns:
+            y_df = y_df.rename(columns={col: col[2:]})
+        # merge x and y nodes and keep only unique nodes
+        out = pd.concat([x_df, y_df], axis=0).query(f'source == "{source}"').drop_duplicates().reset_index(drop=True)
+
+        return out
+
+
+def build_KG(indices, relation, display_relation, x_id, x_type, x_name, x_source, y_id, y_type, y_name, y_source):
+    df = pd.DataFrame('',columns=kg_columns,index=indices)
+
+    df.relation = relation
+    df.display_relation = display_relation
+
+    df.x_id = x_id
+    df.x_type = x_type
+    df.x_name = x_name
+    df.x_source = x_source
+
+    df.y_id = y_id
+    df.y_type = y_type
+    df.y_name = y_name
+    df.y_source = y_source
+
+    kg = KnowledgeGraph(df)
+
+    return kg
