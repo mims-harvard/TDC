c7e8dc72758c38873d0338cea142649aba1c9aee
abearab <abardesigner@gmail.com>
2023-09-30 15:18:10 -0700

    add `knowledge_graph` module

--- tdc/resource/primekg.py
+++ tdc/resource/primekg.py
@@ -5,18 +5,15 @@
 This file contains a primekg dataloader. 
 """
 
-import pandas as pd
 import numpy as np
-import sys
 import warnings
+
+from ..utils import general_load
+from ..knowledge_graph import KnowledgeGraph
 
 warnings.filterwarnings("ignore")
 
-from ..utils import general_load
-
-
 class PrimeKG:
-
     """PrimeKG data loader class to load the knowledge graph with additional support functions."""
 
     def __init__(self, path="./data"):
@@ -37,25 +34,8 @@ class PrimeKG:
             )
         return G
 
-    def get_nodes_by_source(self, source):
-        # extract x nodes
-        x_df = self.df.query(
-            f"x_source == '{source}' | y_source == '{source}'"
-        )[[col for col in self.df.columns if col.startswith("x_")]]
-
-        for col in x_df.columns:
-            x_df = x_df.rename(columns={col: col[2:]})
-
-        # extract y nodes
-        y_df = self.df.query(
-            f"x_source == '{source}' | y_source == '{source}'"
-        )[[col for col in self.df.columns if col.startswith("y_")]]
-        for col in y_df.columns:
-            y_df = y_df.rename(columns={col: col[2:]})
-        # merge x and y nodes and keep only unique nodes
-        out = pd.concat([x_df, y_df], axis=0).query(f'source == "{source}"').drop_duplicates().reset_index(drop=True)
-
-        return out
+    def get_KG(self):
+        return KnowledgeGraph(self.df)
 
     def get_features(self, feature_type):
         if feature_type not in ["drug", "disease"]:
