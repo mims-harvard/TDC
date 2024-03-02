f2b7caa2e245cd8760c57c13d7b73253cc4a6bac
Alejandro Velez <alex@alexv24.com>
2024-03-02 12:52:10 -0500

    makae census api a resource

--- tdc/resource/cellxgene-census.py
+++ tdc/resource/cellxgene-census.py
@@ -2,17 +2,8 @@ import cellxgene_census
 from pandas import concat
 import tiledbsoma
 
-from tdc import base_dataset
-"""
-    
-Are we only supporting memory-efficient queries?
-https://chanzuckerberg.github.io/cellxgene-census/cellxgene_census_docsite_quick_start.html#memory-efficient-queries
 
-
-"""
-
-
-class CXGDataLoader(base_dataset.DataLoader):
+class CensusResource:
 
     def __init__(self,
                  num_slices=None,
@@ -43,9 +34,67 @@ class CXGDataLoader(base_dataset.DataLoader):
         with cellxgene_census.open_soma(
                 census_version=census_version) as census:
             # Reads SOMADataFrame as a slice
-            cell_metadata = census[dataset][organism].obs.read(
-                value_filter=value_filter, column_names=self.column_names)
-            self.df = cell_metadata.concat().to_pandas()
+            print("reading data...")
+            obs = census[dataset][organism].obs
+            print("got var df")
+            print("filtering")
+            varread = obs.read(value_filter=value_filter, column_names = self.column_names)
+            print("converting to pandas.. first pyarrow")
+            cc = varread.concat()
+            print("now to pandas")
+            self.df = cc.to_pandas()
+            print(self.df.head())
+            print("now running var test")
+            
+            var = census[dataset][organism].ms[measurement_name].var
+            self.df = var.read(column_names=["feature_name", "feature_reference"], value_filter="feature_id in ['ENSG00000161798', 'ENSG00000188229']").concat().to_pandas()
+            print("printing var")
+            print(self.df.head())
+            # TODO: gene / var columns are var: 'soma_joinid', 'feature_id', 'feature_name', 'feature_length'
+
+            print("now testing queries on data matrices (X)")
+            n_obs = len(obs)
+            n_var =len(var)
+            X = census[dataset][organism].ms[measurement_name].X["raw"]
+            slc = X.read((slice(0, 5),)).coos((n_obs,n_var)) # need bounding boxes
+            self.df = slc.concat()
+            print(self.df)
+            print("prinnting  X")
+            print(self.df.to_scipy().todense())
+            
+            print("now testing feature dataset presence matrix")
+            fMatrix = census[dataset][organism].ms[measurement_name]["feature_dataset_presence_matrix"]
+            slc = fMatrix.read((slice(0, 5),)).coos((n_obs,n_var)) # need bounding boxes
+            self.df = slc.concat()
+            print(self.df)
+            print("printing ftp matrix")
+            print(self.df.to_scipy().todense())
+            
+            print("can we do full read on X?")
+            bded = X.read().coos((n_obs, n_var)) # still need bounding boxes
+            print("can get the sparse array coos()")
+            print("we cannot get pyarrow")
+            # print("pyarrow")
+            # bded.concat()
+            print("yes we can")
+            
+            # X = census[dataset][organism].ms[measurement_name].X["raw"]
+            # sparse_array = X.read()
+            # print("spare array...")
+            # print(sparse_array)
+            # # TODO: tmp
+            # print("converting to pandas")
+            # print("first to pyarrow")
+            # self.df = sparse_array.coos().concat()
+            # print("done")
+            # print("now pandas")
+            # self.df = self.df.to_pandas()
+            # print("done")
+            
+            
+            # .read(
+            #     value_filter=value_filter, column_names=self.column_names)
+            # self.df = cell_metadata.concat().to_pandas()
             # TODO: not latency on memory-efficient queries is poor...
             # organismCollection = census[dataset][organism]
             # query = organismCollection.axis_query(
@@ -69,10 +118,24 @@ class CXGDataLoader(base_dataset.DataLoader):
             )
         return self.df
 
+    def get_data(self, type="df"):
+        if type == "df":
+            return self.get_dataframe()
+        elif type == "pyarrow":
+            raise Exception("PyArrow format not supported by TDC yet.")
+        else:
+            raise Exception("Type must be set to df or pyarrow")
+
 
 if __name__ == "__main__":
     # TODO: tmp, run testing suite when this file is called as main
-    loader = CXGDataLoader(value_filter="tissue == 'brain' and sex == 'male'",
+    print("initializing object")
+    loader = CensusResource(value_filter="tissue == 'brain' and sex == 'male'",
                            column_names=["assay", "cell_type", "tissue"])
-    df = loader.get_dataframe()
-    print(df.head())
+    print("getting")
+    df = loader.get_data()
+    print("getting head()")
+    # print(df.head())
+    print("no dense")
+    print(df.to_scipy())
+    print("done!")
