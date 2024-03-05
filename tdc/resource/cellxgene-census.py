bb83822714f86f55da00f1ac0a32de60cde52a6d
Alejandro Velez <alex@alexv24.com>
2024-03-04 20:14:43 -0500

    implement memory-efficient retrieval of count matrix and update environment. Also support AnnData retrieval

--- tdc/resource/cellxgene-census.py
+++ tdc/resource/cellxgene-census.py
@@ -3,14 +3,14 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = "TRUE"
 # TODO: find better fix or encode in environment / docker ^^^
 import cellxgene_census
-from functools import wraps
-from pandas import concat
+import gget
 import tiledbsoma
 
 
 class CensusResource:
     
     _CENSUS_DATA = "census_data"
+    _CENSUS_META = "census_info"
     _FEATURE_PRESENCE = "feature_dataset_presence_matrix"
     _LATEST_CENSUS = "2023-12-15"
     _HUMAN = "homo_sapiens"
@@ -21,14 +21,20 @@ class CensusResource:
             # @wraps(func)
             def check(*args, **kwargs):
                 self = args[0]
-                if self.dataset != self._CENSUS_DATA:
-                    raise ValueError("This function requires the '{}' dataset".format(self._CENSUS_DATA))
+                self.dataset = self._CENSUS_DATA
+                return func(*args, **kwargs)
+            return check
+
+        @classmethod
+        def check_dataset_is_census_info(cls,func):
+            def check(*args, **kwargs):
+                self = args[0]
+                self.dataset = self._CENSUS_META
                 return func(*args, **kwargs)
             return check
 
     def __init__(self,
                  census_version=None,
-                 dataset=None,
                  organism=None
     ):
         """Initialize the Census Resource.
@@ -38,8 +44,8 @@ class CensusResource:
             TODO: complete
         """
         self.census_version = census_version if census_version is not None else self._LATEST_CENSUS
-        self.dataset = dataset if dataset is not None else self._CENSUS_DATA
         self.organism = organism if organism is not None else self._HUMAN
+        self.dataset = None # variable to set target census collection to either info or data
 
     def fmt_cellxgene_data(self, tiledb_ptr, fmt=None):
         if fmt is None:
@@ -149,6 +155,138 @@ class CensusResource:
             out = self.fmt_cellxgene_data(slc, fmt)
             return out if not todense else out.todense()
 
+    @decorators.check_dataset_is_census_info
+    def get_metadata(self):
+        """Get the metadata for the Cell Census."""
+        with cellxgene_census.open_soma(
+            census_version=self.census_version
+        ) as census:
+            return census[self.dataset]["summary"]
+
+    @decorators.check_dataset_is_census_info
+    def get_dataset_metadata(self):
+        """Get the metadata for the Cell Census's datasets."""
+        with cellxgene_census.open_soma(
+            census_version=self.census_version
+        ) as census:
+            return census[self.dataset]["datasets"]
+        
+    @decorators.check_dataset_is_census_info
+    def get_cell_count_metadata(self):
+        """Get the cell counts across cell metadata for the Cell Census."""
+        with cellxgene_census.open_soma(
+            census_version=self.census_version
+        ) as census:
+            return census[self.dataset]["summary_cell_counts"]
+
+    @decorators.check_dataset_is_census_data
+    def query_measurement_matrix(
+        self,
+        value_filter=None,
+        value_adjustment=None,
+        measurement_name=None,
+        fmt=None,
+        todense=None
+    ):
+        """Query the Census Measurement Matrix. Function returns a Python generator.
+
+        Args:
+            value_filter (_type_, optional): _description_. Defaults to None.
+            value_adjustment (_type_, optional): _description_. Defaults to None.
+            measurement_name (_type_, optional): _description_. Defaults to None.
+            fmt (_type_, optional): _description_. Defaults to None.
+            todense (_type_, optional): _description_. Defaults to None.
+
+        Raises:
+            ValueError: _description_
+            Exception: _description_
+            ValueError: _description_
+            ValueError: _description_
+            
+        Yields:
+            a slice of the output query in the specified format
+        """
+        if value_filter is None:
+            raise ValueError("query_measurement_matrix expects a value_filter. if you don't plan to apply a filter, use get_measurement_matrix()")
+        elif measurement_name is None:
+            raise Exception("measurement_name was not provided.")
+        elif fmt is not None and fmt not in ["scipy", "pyarrow"]:
+            raise ValueError("measurement_matrix only supports 'scipy' or 'pyarrow' format")
+        value_adjustment = value_adjustment if value_adjustment is not None else "raw"
+        todense = todense if todense is not None else False
+        fmt = fmt if fmt is not None else "scipy"
+        if todense and fmt != "scipy":
+            raise ValueError("dense representation only available in scipy format")
+        with cellxgene_census.open_soma(
+            census_version=self.census_version
+        ) as census:
+            organism = census[self.dataset][self.organism]
+            query = organism.axis_query(
+                measurement_name = measurement_name,
+                obs_query = tiledbsoma.AxisQuery(
+                    value_filter = value_filter
+                )
+            )
+            it = query.X(value_adjustment).tables()
+            for slc in it:
+                out = self.fmt_cellxgene_data(slc, fmt)
+                out = out if not todense else out.todense()
+                yield out
+        
+
+    @classmethod
+    def gget_czi_cellxgene(cls, **kwargs):
+        """Wrapper for cellxgene gget()
+            https://chanzuckerberg.github.io/cellxgene-census/notebooks/api_demo/census_gget_demo.html
+            Support for AnnData or DataFrame. Params included below
+            
+            General args:
+            - species        Choice of 'homo_sapiens' or 'mus_musculus'. Default: 'homo_sapiens'.
+            - gene           Str or list of gene name(s) or Ensembl ID(s), e.g. ['ACE2', 'SLC5A1'] or ['ENSG00000130234', 'ENSG00000100170']. Default: None.
+                            NOTE: Set ensembl=True when providing Ensembl ID(s) instead of gene name(s).
+                            See https://cellxgene.cziscience.com/gene-expression for examples of available genes.
+            - ensembl        True/False (default: False). Set to True when genes are provided as Ensembl IDs.
+            - column_names   List of metadata columns to return (stored in AnnData.obs when meta_only=False).
+                            Default: ["dataset_id", "assay", "suspension_type", "sex", "tissue_general", "tissue", "cell_type"]
+                            For more options see: https://api.cellxgene.cziscience.com/curation/ui/#/ -> Schemas -> dataset
+            - meta_only      True/False (default: False). If True, returns only metadata dataframe (corresponds to AnnData.obs).
+            - census_version Str defining version of Census, e.g. "2023-05-15" or "latest" or "stable". Default: "stable".
+            - verbose        True/False whether to print progress information. Default True.
+            - out            If provided, saves the generated AnnData h5ad (or csv when meta_only=True) file with the specified path. Default: None.
+
+        Cell metadata attributes:
+            - tissue                          Str or list of tissue(s), e.g. ['lung', 'blood']. Default: None.
+                                            See https://cellxgene.cziscience.com/gene-expression for examples of available tissues.
+            - cell_type                       Str or list of celltype(s), e.g. ['mucus secreting cell', 'neuroendocrine cell']. Default: None.
+                                            See https://cellxgene.cziscience.com/gene-expression and select a tissue to see examples of available celltypes.
+            - development_stage               Str or list of development stage(s). Default: None.
+            - disease                         Str or list of disease(s). Default: None.
+            - sex                             Str or list of sex(es), e.g. 'female'. Default: None.
+            - is_primary_data                 True/False (default: True). If True, returns only the canonical instance of the cellular observation.
+                                            This is commonly set to False for meta-analyses reusing data or for secondary views of data.
+            - dataset_id                      Str or list of CELLxGENE dataset ID(s). Default: None.
+            - tissue_general_ontology_term_id Str or list of high-level tissue UBERON ID(s). Default: None.
+                                            Also see: https://github.com/chanzuckerberg/single-cell-data-portal/blob/9b94ccb0a2e0a8f6182b213aa4852c491f6f6aff/backend/wmg/data/tissue_mapper.py
+            - tissue_general                  Str or list of high-level tissue label(s). Default: None.
+                                            Also see: https://github.com/chanzuckerberg/single-cell-data-portal/blob/9b94ccb0a2e0a8f6182b213aa4852c491f6f6aff/backend/wmg/data/tissue_mapper.py
+            - tissue_ontology_term_id         Str or list of tissue ontology term ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - assay_ontology_term_id          Str or list of assay ontology term ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - assay                           Str or list of assay(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - cell_type_ontology_term_id      Str or list of celltype ontology term ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - development_stage_ontology_term_id        Str or list of development stage ontology term ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - disease_ontology_term_id        Str or list of disease ontology term ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - donor_id                        Str or list of donor ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - self_reported_ethnicity_ontology_term_id  Str or list of self reported ethnicity ontology ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - self_reported_ethnicity         Str or list of self reported ethnicity as defined in the CELLxGENE dataset schema. Default: None.
+            - sex_ontology_term_id            Str or list of sex ontology ID(s) as defined in the CELLxGENE dataset schema. Default: None.
+            - suspension_type                 Str or list of suspension type(s) as defined in the CELLxGENE dataset schema. Default: None.
+
+        Returns AnnData object (when meta_only=False) or dataframe (when meta_only=True).
+
+        """
+        gget.setup("cellxgene")
+        return gget.cellxgene(**kwargs)
+
 
 if __name__ == "__main__":
     # TODO: tmp, run testing suite when this file is called as main
@@ -158,7 +296,7 @@ if __name__ == "__main__":
     cell_value_filter = "tissue == 'brain' and sex == 'male'"
     cell_column_names = ["assay", "cell_type", "tissue"]
     gene_value_filter = "feature_id in ['ENSG00000161798', 'ENSG00000188229']"
-    gene_column_names = ["feature_name", "feature_reference"]
+    gene_column_names = ["feature_name", "feature_length"]
     print("getting cell metadata as pandas dataframe")
     obsdf = resource.get_cell_metadata(value_filter=cell_value_filter, column_names=cell_column_names, fmt="pandas")
     print("success!")
