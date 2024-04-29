12a8fba20dba47126d8ba064d09b60445bc7623f
abearab <abardesigner@gmail.com>
2024-04-28 23:42:05 -0700

    fix formating

--- tdc/utils/knowledge_graph.py
+++ tdc/utils/knowledge_graph.py
@@ -29,20 +29,25 @@ class KnowledgeGraph:
     def get_nodes_by_source(self, source):
         # extract x nodes
         x_df = self.df.query(
-            f"x_source == '{source}' | y_source == '{source}'"
-        )[[col for col in self.df.columns if col.startswith("x_")]]
+            f"x_source == '{source}' | y_source == '{source}'")[[
+                col for col in self.df.columns if col.startswith("x_")
+            ]]
 
         for col in x_df.columns:
             x_df = x_df.rename(columns={col: col[2:]})
 
         # extract y nodes
         y_df = self.df.query(
-            f"x_source == '{source}' | y_source == '{source}'"
-        )[[col for col in self.df.columns if col.startswith("y_")]]
+            f"x_source == '{source}' | y_source == '{source}'")[[
+                col for col in self.df.columns if col.startswith("y_")
+            ]]
         for col in y_df.columns:
             y_df = y_df.rename(columns={col: col[2:]})
         # merge x and y nodes and keep only unique nodes
-        out = pd.concat([x_df, y_df], axis=0).query(f'source == "{source}"').drop_duplicates().reset_index(drop=True)
+        out = pd.concat([
+            x_df, y_df
+        ], axis=0).query(f'source == "{source}"').drop_duplicates().reset_index(
+            drop=True)
 
         return out
 
