91ee851f92ba0031ee911871c4c67bbfd152330e
Alejandro Velez <alex@alexv24.com>
2024-05-13 11:51:58 -0400

    mend

--- tdc/dataset_configs/opentargets_dti.py
+++ tdc/dataset_configs/opentargets_dti.py
@@ -8,48 +8,53 @@ class OpentargetsDTI(ResourceConfig):
     def __init__(self):
         super(OpentargetsDTI, self).__init__(
             ResourceFeatureGenerator(),
-            keys=["opentargets_ra_data_splits", "opentargets_ibd_data_splits", "df"],
+            keys=[
+                "opentargets_ra_data_splits", "opentargets_ibd_data_splits",
+                "df"
+            ],
             loader_functions=["split", "split", "concat"],
             loader_args=[{
                 "dataset": ("self", "opentargets_ra_drug_evidence"),
                 "column_name":
                     "targetId_genename",
-                "pos_train":
-                    ("self",
-                     ["opentargets_ra_data_splits", "splits", "pos_train_names"]),
+                "pos_train": ("self", [
+                    "opentargets_ra_data_splits", "splits", "pos_train_names"
+                ]),
                 "pos_dev":
                     None,
                 "pos_test":
                     ("self",
-                     ["opentargets_ra_data_splits", "splits", "pos_test_names"]),
-                "neg_train":
-                    ("self",
-                     ["opentargets_ra_data_splits", "splits", "neg_train_names"]),
+                     ["opentargets_ra_data_splits", "splits",
+                      "pos_test_names"]),
+                "neg_train": ("self", [
+                    "opentargets_ra_data_splits", "splits", "neg_train_names"
+                ]),
                 "neg_dev":
                     None,
                 "neg_test":
                     ("self",
-                     ["opentargets_ra_data_splits", "splits", "neg_test_names"]),
+                     ["opentargets_ra_data_splits", "splits",
+                      "neg_test_names"]),
             }, {
                 "dataset": ("self", "opentargets_ibd_drug_evidence"),
                 "column_name":
                     "targetId_genename",
-                "pos_train":
-                    ("self",
-                     ["opentargets_ibd_data_splits", "splits", "pos_train_names"]),
+                "pos_train": ("self", [
+                    "opentargets_ibd_data_splits", "splits", "pos_train_names"
+                ]),
                 "pos_dev":
                     None,
-                "pos_test":
-                    ("self",
-                     ["opentargets_ibd_data_splits", "splits", "pos_test_names"]),
-                "neg_train":
-                    ("self",
-                     ["opentargets_ibd_data_splits", "splits", "neg_train_names"]),
+                "pos_test": ("self", [
+                    "opentargets_ibd_data_splits", "splits", "pos_test_names"
+                ]),
+                "neg_train": ("self", [
+                    "opentargets_ibd_data_splits", "splits", "neg_train_names"
+                ]),
                 "neg_dev":
                     None,
-                "neg_test":
-                    ("self",
-                     ["opentargets_ibd_data_splits", "splits", "neg_test_names"])
+                "neg_test": ("self", [
+                    "opentargets_ibd_data_splits", "splits", "neg_test_names"
+                ])
             }, {
                 "ds_list": [
                     "opentargets_ibd_data_splits", "opentargets_ra_data_splits"
