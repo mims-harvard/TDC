873592ba24e413e5cb84537844dac10a1aa62024
Alejandro Velez <alex@alexv24.com>
2024-06-04 10:23:41 -0400

    fix syntax errors

--- tdc/benchmark_group/geneperturb_group.py
+++ tdc/benchmark_group/geneperturb_group.py
@@ -21,7 +21,10 @@ class GenePerturbGroup(CounterfactualGroup):
         'perturbation', 'perturbation_type', 'tissue_type'
     ]
 
-    def __init__(self, dataset="scperturb_gene_ReplogleWeissman2022_k562_essential", path="./data", file_format="csv"):
+    def __init__(self,
+                 dataset="scperturb_gene_ReplogleWeissman2022_k562_essential",
+                 path="./data",
+                 file_format="csv"):
         """Create a GenePerturbGroup prediction benchmark group class."""
         self.name = "GenePerturbGroup"
         self.path = os.path.join(path, self.name)
@@ -31,7 +34,7 @@ class GenePerturbGroup(CounterfactualGroup):
             "scperturb_gene_ReplogleWeissman2022_k562_essential"
         ]
         self.file_format = file_format
-        
+
         from ..multi_pred.perturboutcome import PerturbOutcome
         dataset = dataset
         assert dataset in self.dataset_names, "{} dataset not in {}".format(
@@ -39,12 +42,14 @@ class GenePerturbGroup(CounterfactualGroup):
         data = PerturbOutcome(dataset)
         self.split = data.get_split()
         self.test = self.get_test()
-        
+
     def get_train_valid_split(self):
         """parameters included for compatibility. this benchmark has a fixed train/test split."""
         train = self.split[0]["train"]
         val = self.split[0]["dev"]
-        self.ctrl = np.array(np.mean(train[train.obs.condition == 'ctrl'].X, axis = 0)).reshape(-1,)
+        self.ctrl = np.array(
+            np.mean(train[train.obs.condition == 'ctrl'].X,
+                    axis=0)).reshape(-1,)
         return train, val
 
     def get_test(self):
@@ -58,7 +63,7 @@ class GenePerturbGroup(CounterfactualGroup):
         mean_truth = {}
         for condition in test.obs.condition.unique():
             out = test[test.obs.condition == condition].X
-            mean_truth[condition] = np.array(np.mean(out, axis = 0)).reshape(-1,)
+            mean_truth[condition] = np.array(np.mean(out, axis=0)).reshape(-1,)
 
         pearson_delta = []
         for i, j in mean_truth.items():
@@ -67,17 +72,29 @@ class GenePerturbGroup(CounterfactualGroup):
         pearson_delta = np.mean(pearson_delta)
 
         A = test.var.gene_name.values
-        de_20 = {i.split('_')[1]:  np.array([np.where(A == b)[0][0] if b in A else -1 for b in j[:20]]) for i,j in test.uns['rank_genes_groups_cov_all'].items()}
+        de_20 = {
+            i.split('_')[1]:
+                np.array(
+                    [np.where(A == b)[0][0] if b in A else -1 for b in j[:20]])
+            for i, j in test.uns['rank_genes_groups_cov_all'].items()
+        }
         mse_20_de = []
         for i, j in mean_truth.items():
             mse_20_de.append(np.mean((pred[i][de_20[i]] - j[de_20[i]])**2))
         mse_20_de = np.mean(mse_20_de)
-        
+
         direction = []
         for i, j in mean_truth.items():
-            direction.append(sum(np.sign(pred[i][de_20[i]] - ctrl[de_20[i]]) == np.sign(j[de_20[i]] - ctrl[de_20[i]]))/20)
+            direction.append(
+                sum(
+                    np.sign(pred[i][de_20[i]] -
+                            ctrl[de_20[i]]) == np.sign(j[de_20[i]] -
+                                                       ctrl[de_20[i]])) / 20)
         direction = np.mean(direction)
 
-        return {'pearson_delta_all_genes': pearson_delta,
-                'mse_20_de_genes': mse_20_de,
-                'ratio_direction_correct_20_de_genes': direction}
+        return {
+            'pearson_delta_all_genes': pearson_delta,
+            'mse_20_de_genes': mse_20_de,
+            'ratio_direction_correct_20_de_genes': direction
+        }
+
