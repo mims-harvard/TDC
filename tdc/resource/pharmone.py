c0d27c25b55c60009afc844b1d25b0ec1710ba56
Alejandro Velez <alex@alexv24.com>
2025-01-02 19:24:43 -0500

    pharmone

--- tdc/resource/pharmone.py
+++ tdc/resource/pharmone.py
@@ -1,45 +1,29 @@
-from ..utils.load import download_wrapper
-
+from ..utils import general_load
 """
 Resource class for the Eve Bio (https://evebio.org/) Pharmone Map.
 """
 
+
 class PharmoneMap(object):
-    
+
     def __init__(self, path="./data"):
         self.path = path
-    
+
     def get_data(self):
-        return download_wrapper(
-            'evebio_pharmone_v1_detailed_result_table',
-            self.path,
-            'evebio_pharmone_v1_detailed_result_table'
-        )  # Load the Pharmone Map data
-        
+        return general_load('evebio_pharmone_v1_detailed_result_table',
+                            self.path, '\t')  # Load the Pharmone Map data
+
     def get_obs_metadata(self):
-        return download_wrapper(
-            "evebio_pharmone_v1_observed_points_table",
-            self.path,
-            "evebio_pharmone_v1_observed_points_table"
-        )
-        
+        return general_load("evebio_pharmone_v1_observed_points_table",
+                            self.path, "\t")
+
     def get_control_data(self):
-        return download_wrapper(
-            "evebio_pharmone_v1_control_table",
-            self.path,
-            "evebio_pharmone_v1_control_table"
-        )  # Load the control data
-    
+        return general_load("evebio_pharmone_v1_control_table", self.path,
+                            "\t")  # Load the control data
+
     def get_compound_data(self):
-        return download_wrapper(
-            "evebio_pharmone_v1_compound_table",
-            self.path,
-            "evebio_pharmone_v1_compound_table"
-        )
-        
+        return general_load("evebio_pharmone_v1_compound_table", self.path,
+                            "\t")
+
     def get_target_data(self):
-        return download_wrapper(
-            "evebio_pharmone_v1_target_table",
-            self.path,
-            "evebio_pharmone_v1_target_table"
-        )
+        return general_load("evebio_pharmone_v1_target_table", self.path, "\t")
--- tdc/test/test_resources.py
+++ tdc/test/test_resources.py
@@ -125,23 +125,23 @@ class TestPINNACLE(unittest.TestCase):
         except:
             pass
 
+
 class TestPharmoneMap(unittest.TestCase):
 
     def setUp(self):
         print(os.getcwd())
         pass
-    
+
     def test_get_data(self):
         from tdc.resource.pharmone import PharmoneMap
-        
+
         resource = PharmoneMap()
         data = resource.get_data()
-        assert data
-        assert isinstance(data, DataFrame)
+        assert isinstance(data, DataFrame), type(data)
         assert "Compound" in data.columns
         assert "Target_ID" in data.columns
         assert "pXC50" in data.columns
-        
+
     def tearDown(self):
         try:
             print(os.getcwd())
