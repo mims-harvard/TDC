6ef92ecd55f9fc4ec7bd67668a9c04b71c87fdc9
Alejandro Velez <alex@alexv24.com>
2025-01-20 17:29:08 -0500

    add geneformer scperturb test

--- tdc/test/test_model_server.py
+++ tdc/test/test_model_server.py
@@ -55,6 +55,59 @@ class TestModelServer(unittest.TestCase):
                             attention_mask=mask)
         print(f"scgpt ran successfully. here is an output {first_embed}")
 
+    def testGeneformerPerturb(self):
+        from tdc.multi_pred.perturboutcome import PerturbOutcome
+        dataset = "scperturb_drug_AissaBenevolenskaya2021"
+        data = PerturbOutcome(dataset)
+        adata = data.adata
+        tokenizer = GeneformerTokenizer()
+        adata.var["feature_id"] = adata.var.index.map(
+            lambda x: tokenizer.gene_name_id_dict.get(x, 0))
+        x = tokenizer.tokenize_cell_vectors(adata,
+                                            ensembl_id="feature_id",
+                                            ncounts="ncounts")
+        cells, _ = x
+        assert cells, "FAILURE: cells false-like. Value is = {}".format(cells)
+        assert len(cells) > 0, "FAILURE: length of cells <= 0 {}".format(cells)
+        from tdc import tdc_hf_interface
+        import torch
+        geneformer = tdc_hf_interface("Geneformer")
+        model = geneformer.load()
+        input_tensor = torch.tensor(cells)
+        out = []
+        try:
+            ctr = 0  # stop after some passes to avoid failure
+            for batch in input_tensor:
+                # build an attention mask
+                attention_mask = torch.tensor(
+                    [[x[0] != 0, x[1] != 0] for x in batch])
+                outputs = model(batch,
+                                attention_mask=attention_mask,
+                                output_hidden_states=True)
+                layer_to_quant = quant_layers(model) + (
+                    -1
+                )  # TODO note this can be parametrized to either 0 (extract last embedding layer) or -1 (second-to-last which is more generalized)
+                embs_i = outputs.hidden_states[layer_to_quant]
+                # there are "cls", "cell", and "gene" embeddings. we will only capture "gene", which is cell type specific. for "cell", you'd average out across unmasked gene embeddings per cell
+                embs = embs_i
+                out.append(embs)
+                if ctr == 2:
+                    break
+                ctr += 1
+        except Exception as e:
+            raise Exception(e)
+
+        assert out, "FAILURE: Geneformer output is false-like. Value = {}".format(
+            out)
+        assert len(
+            out
+        ) == 3, "length not matching ctr+1: {} vs {}. output was \n {}".format(
+            len(out), ctr + 1, out)
+        print(
+            "Geneformer ran sucessfully. Find batch embedding example here:\n {}"
+            .format(
+                out[0]))  # TODO: test for cell-type-specific emebdding count
+
     def testGeneformerTokenizer(self):
 
         adata = self.resource.get_anndata(
