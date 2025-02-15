c78bf54c7d8f3c795fef8190a00c6ec3d09fe991
jesusCaraball0 <jesuscaraballotsic@gmail.com>
2025-02-15 17:38:40 -0500

    forgot a quotation mark

--- tdc/model_server/model_loaders/scvi_loader.py
+++ tdc/model_server/model_loaders/scvi_loader.py
@@ -6,8 +6,7 @@ class scVILoader():
         import requests
         import os
 
-        scvi_url = f"https://cellxgene-contrib-public.s3.us-west-2.amazonaws.com/
-        models/scvi/{census_version}/homo_sapiens/model.pt"
+        scvi_url = f"https://cellxgene-contrib-public.s3.us-west-2.amazonaws.com/models/scvi/{census_version}/homo_sapiens/model.pt"
         os.makedirs(os.path.join(os.getcwd(), 'scvi_model'), exist_ok=True)
 
         output_path = os.path.join('scvi_model', 'model.pt')
@@ -19,8 +18,7 @@ class scVILoader():
         except Exception as e:
             print(e)
             census_version = "2024-07-01"
-            scvi_url = f"https://cellxgene-contrib-public.s3.us-west-2.amazonaws.com/
-            models/scvi/2024-07-01/homo_sapiens/model.pt"
+            scvi_url = f"https://cellxgene-contrib-public.s3.us-west-2.amazonaws.com/models/scvi/2024-07-01/homo_sapiens/model.pt"
             response = requests.get(scvi_url, verify=False)
 
         with open(output_path, "wb") as file:
